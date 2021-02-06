dfdbd9badb58ef55b388e58ed249efa2a1cf2c30
Vitali Griunvald <vgriunvald@gmail.com>
2021-02-07 01:09:22 +0200

    Fix - commit unsaved changes in app.js

--- server/app.js
+++ server/app.js
@@ -4,10 +4,8 @@ const cors = require('cors');
 const app = express();
 const helmet = require('helmet');
 const morgan = require('morgan');
-const admin = require('firebase-admin');
 
 const authRoutes = require('./routes/auth_routes');
-const serviceAccount = require('./config/serviceAccountKey.json');
 
 app.use(helmet());
 app.use(express.json());
@@ -18,10 +16,6 @@ app.use(
     origin: process.env.CORS_URL,
   })
 );
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
 
 authRoutes(app);
 
