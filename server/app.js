426bcfe36d3d3fdda072273ff52fda9e9e309f2e
Vitali Griunvald <vgriunvald@gmail.com>
2021-02-07 00:38:33 +0200

    Install and configure Firebase admin SDK

--- server/app.js
+++ server/app.js
@@ -4,11 +4,12 @@ const cors = require('cors');
 const app = express();
 const helmet = require('helmet');
 const morgan = require('morgan');
+const admin = require('firebase-admin');
 
 const authRoutes = require('./routes/auth_routes');
+const serviceAccount = require('./config/serviceAccountKey.json');
 
 app.use(helmet());
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true, useUnifiedTopology: true }));
 app.use(morgan('dev'));
@@ -17,6 +18,10 @@ app.use(
     origin: process.env.CORS_URL,
   })
 );
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
 
 authRoutes(app);
 
