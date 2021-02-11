2b2ef819e41051a0ac4c87bc64edc48856dcbf70
Vitali Griunvald <vgriunvald@gmail.com>
2021-02-11 12:08:23 +0200

    Change date picker to calendar

--- client/src/components/form/RequestForm.js
+++ client/src/components/form/RequestForm.js
@@ -1,17 +1,17 @@
 import React, { useState } from 'react';
 import 'react-modern-calendar-datepicker/lib/DatePicker.css';
-import DatePicker from 'react-modern-calendar-datepicker';
+import { Calendar } from 'react-modern-calendar-datepicker';
 
 const RequestForm = () => {
+  // ✅ a change in default state: { from: ..., to: ... }
   const [selectedDayRange, setSelectedDayRange] = useState({
     from: null,
     to: null,
   });
   return (
-    <DatePicker
+    <Calendar
       value={selectedDayRange}
       onChange={setSelectedDayRange}
-      inputPlaceholder="Select a day range"
       shouldHighlightWeekends
     />
   );
