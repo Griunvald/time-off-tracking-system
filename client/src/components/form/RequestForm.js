da21751b73638b5f953250730f917eb38a520c65
Vitali Griunvald <vgriunvald@gmail.com>
2021-02-13 23:42:56 +0200

    Restore calendar

--- client/src/components/form/RequestForm.js
+++ client/src/components/form/RequestForm.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import 'react-modern-calendar-datepicker/lib/DatePicker.css';
+import { Calendar } from 'react-modern-calendar-datepicker';
 
 // import { setSelectedDayRange } from '../../actions/calendarActions';
 
@@ -7,7 +9,13 @@ const RequestForm = () => {
   const dispatch = useDispatch();
   const selectedDayRange = useSelector((state) => state.calendar.range);
   console.log(selectedDayRange);
-  return <div>Reaues form</div>;
+  return (
+    <Calendar
+      value={selectedDayRange}
+      onChange={(v) => dispatch({ type: 'SET_SELECTED_DAY_RANGE', payload: v })}
+      shouldHighlightWeekends
+    />
+  );
 };
 
 export default RequestForm;
