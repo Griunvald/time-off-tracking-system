4addcc3c4a11cd64157f49272234043dec2295e4
Vitali Griunvald <vgriunvald@gmail.com>
2021-02-14 02:03:53 +0200

    Disable submit button on request form is dates not selected

--- client/src/components/form/RequestForm.js
+++ client/src/components/form/RequestForm.js
@@ -1,8 +1,5 @@
 import React from 'react';
-import { Form, Formik } from 'formik';
-import { Button, Label } from 'semantic-ui-react';
-import DynamicTextArea from './DynamicTextArea';
-import * as Yup from 'yup';
+import { Form, Button, TextArea } from 'semantic-ui-react';
 import { useDispatch, useSelector } from 'react-redux';
 import 'react-modern-calendar-datepicker/lib/DatePicker.css';
 import { Calendar } from 'react-modern-calendar-datepicker';
@@ -13,6 +10,10 @@ const RequestForm = () => {
   const dispatch = useDispatch();
   const selectedDayRange = useSelector((state) => state.calendar.range);
   console.log(selectedDayRange);
+  let isDisabled;
+  if (selectedDayRange.from === null || selectedDayRange.to === null) {
+    isDisabled = true;
+  }
   return (
     <>
       <Calendar
@@ -22,48 +23,19 @@ const RequestForm = () => {
         }
         shouldHighlightWeekends
       />
+      <Form>
+        <TextArea label="About" placeholder="Tell us more about you..." />
 
-      <Formik
-        initialValues={{ reason: '' }}
-        validationSchema={Yup.object({
-          reason: Yup.string(),
-        })}
-        onSubmit={async (values, { setSubmitting, setErrors }) => {
-          try {
-            console.log(values);
-            setSubmitting(false);
-          } catch (error) {
-            console.log(error);
-          }
-        }}
-      >
-        {({ isSubmitting, isValid, dirty, errors }) => (
-          <Form className="ui form" autoComplete="off">
-            <DynamicTextArea
-              name="reason"
-              placeholder="Please, provide a reason for time off"
-            />
-
-            {errors.auth && (
-              <Label
-                basic
-                color="red"
-                style={{ marginBottom: '10px' }}
-                content={errors.auth}
-              />
-            )}
-            <Button
-              loading={isSubmitting}
-              disabled={!isValid || isSubmitting}
-              type="submit"
-              fluid
-              size="large"
-              color="green"
-              content="Send"
-            />
-          </Form>
-        )}
-      </Formik>
+        <Button
+          loading={false}
+          disabled={isDisabled}
+          type="submit"
+          fluid
+          size="large"
+          color="green"
+          content="Send"
+        />
+      </Form>
     </>
   );
 };
