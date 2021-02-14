29785127934bc3e11097f934f501c808725973b9
Vitali Griunvald <vgriunvald@gmail.com>
2021-02-14 13:16:13 +0200

    Add dynamic text area to request form

--- client/src/components/form/RequestForm.js
+++ client/src/components/form/RequestForm.js
@@ -1,42 +1,51 @@
 import React from 'react';
-import { Form, Button, TextArea } from 'semantic-ui-react';
-import { useDispatch, useSelector } from 'react-redux';
-import 'react-modern-calendar-datepicker/lib/DatePicker.css';
-import { Calendar } from 'react-modern-calendar-datepicker';
-
-// import { setSelectedDayRange } from '../../actions/calendarActions';
+import { Button, Label } from 'semantic-ui-react';
+import { useDispatch } from 'react-redux';
+import { Form, Formik } from 'formik';
+import * as Yup from 'yup';
+import DynamicTextArea from './DynamicTextArea';
 
 const RequestForm = () => {
   const dispatch = useDispatch();
-  const selectedDayRange = useSelector((state) => state.calendar.range);
-  console.log(selectedDayRange);
-  let isDisabled;
-  if (selectedDayRange.from === null || selectedDayRange.to === null) {
-    isDisabled = true;
-  }
   return (
-    <>
-      <Calendar
-        value={selectedDayRange}
-        onChange={(v) =>
-          dispatch({ type: 'SET_SELECTED_DAY_RANGE', payload: v })
-        }
-        shouldHighlightWeekends
-      />
-      <Form>
-        <TextArea label="About" placeholder="Tell us more about you..." />
-
-        <Button
-          loading={false}
-          disabled={isDisabled}
-          type="submit"
-          fluid
-          size="large"
-          color="green"
-          content="Send"
-        />
-      </Form>
-    </>
+    <div>
+      <Formik
+        initialValues={{ text: '' }}
+        validationSchema={Yup.object({
+          text: Yup.string().required(),
+        })}
+        onSubmit={async (values, { setSubmitting, setErrors }) => {
+          try {
+            setSubmitting(false);
+          } catch (error) {
+            setErrors({ auth: 'Invalid email or password' });
+          }
+        }}
+      >
+        {({ isSubmitting, isValid, dirty, errors }) => (
+          <Form className="ui form" autoComplete="off">
+            <DynamicTextArea name="text" />
+            {errors.auth && (
+              <Label
+                basic
+                color="red"
+                style={{ marginBottom: '10px' }}
+                content={errors.auth}
+              />
+            )}
+            <Button
+              loading={isSubmitting}
+              disabled={!isValid || !dirty || isSubmitting}
+              type="submit"
+              fluid
+              size="large"
+              color="green"
+              content="Send"
+            />
+          </Form>
+        )}
+      </Formik>
+    </div>
   );
 };
 
