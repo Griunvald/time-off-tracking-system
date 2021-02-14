4a57586101675d413381046a724f856324f86417
Vitali Griunvald <vgriunvald@gmail.com>
2021-02-14 21:02:49 +0200

    Add start date and end date with error messages to the request form

--- client/src/components/form/DynamicDateInput.js
+++ client/src/components/form/DynamicDateInput.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { useField, useFormikContext } from 'formik';
 import DatePicker from 'react-datepicker';
 
-import { FormField } from 'semantic-ui-react';
+import { FormField, Label } from 'semantic-ui-react';
 import 'react-datepicker/dist/react-datepicker.css';
 
 const DynamicDateInput = ({ label, ...props }) => {
@@ -10,22 +10,19 @@ const DynamicDateInput = ({ label, ...props }) => {
   const [field, meta] = useField(props);
   return (
     <FormField error={meta.touched && !!meta.error}>
-      <label>{label}</label>
       <DatePicker
         {...field}
         {...props}
         selected={(field.value && new Date(field.value)) || null}
         onChange={(value) => setFieldValue(field.name, value)}
       />
+      {meta.touched && meta.error ? (
+        <Label basic color="red">
+          {meta.error}
+        </Label>
+      ) : null}
     </FormField>
   );
 };
 
 export default DynamicDateInput;
-// {
-//   meta.touched && meta.error ? (
-//     <Label basic color="red">
-//       {meta.error}
-//     </Label>
-//   ) : null;
-// }
--- client/src/components/form/RequestForm.js
+++ client/src/components/form/RequestForm.js
@@ -8,13 +8,15 @@ import DynamicDateInput from './DynamicDateInput';
 
 const RequestForm = () => {
   const dispatch = useDispatch();
+
   return (
     <div>
       <Formik
         initialValues={{ text: '', date: '' }}
         validationSchema={Yup.object({
           text: Yup.string().required(),
-          date: Yup.string().required(),
+          'start date': Yup.string().required(),
+          'end date': Yup.string().required(),
         })}
         onSubmit={async (values, { setSubmitting, setErrors }) => {
           try {
@@ -27,7 +29,22 @@ const RequestForm = () => {
       >
         {({ isSubmitting, isValid, dirty, errors }) => (
           <Form className="ui form" autoComplete="off">
-            <DynamicDateInput name="date" />
+            <DynamicDateInput
+              name="start date"
+              selected={1}
+              onChange={1}
+              startDate={1}
+              endDate={1}
+              placeholderText="Select start date"
+            />
+            <DynamicDateInput
+              name="end date"
+              selected={1}
+              onChange={1}
+              startDate={1}
+              endDate={1}
+              placeholderText="Select end date"
+            />
             <DynamicTextArea name="text" />
             {errors.auth && (
               <Label
