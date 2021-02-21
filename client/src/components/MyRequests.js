b36a83193e7ddf4ba7f183c4e24da221c64da217
Vitali Griunvald <vgriunvald@gmail.com>
2021-02-21 02:12:13 +0200

    Create firebaseLooper utility, refactor MyRequests component

--- client/src/components/MyRequests.js
+++ client/src/components/MyRequests.js
@@ -2,26 +2,26 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import firebase from './../config/firebase';
 import { Table, Icon, Label } from 'semantic-ui-react';
+import { firebaseLooper } from '../utils/firebaseLooper';
 const moment = require('moment');
 const db = firebase.firestore();
 
 const MyRequests = () => {
+  const dispatch = useDispatch();
   useEffect(() => {
     const requestRef = db
       .collection('users_requests')
       .doc('user_one@mail.com')
       .collection('user_one@mail.com');
 
-    requestRef.onSnapshot((snapshot) => {
-      let arr = [];
-      snapshot.forEach((doc) => arr.push(doc.data()));
-      dispatch({ type: 'GET_USER_REQUESTS', payload: arr });
+    requestRef.get().then((snapshot) => {
+      const requests = firebaseLooper(snapshot);
+      console.log(requests);
+      dispatch({ type: 'GET_USER_REQUESTS', payload: requests });
     });
   }, []);
-  const dispatch = useDispatch();
-  let requestsData = [];
-  requestsData.push(useSelector((state) => state.userRequests.requests));
-  console.log(requestsData);
+
+  const requests = useSelector((state) => state.userRequests.requests);
 
   return (
     <div>
@@ -37,7 +37,7 @@ const MyRequests = () => {
           </Table.Row>
         </Table.Header>
         <Table.Body>
-          {requestsData[0].map((item) => (
+          {requests.map((item) => (
             <Table.Row>
               <Table.Cell>
                 {moment.unix(item.createdAt.seconds).toString().slice(3, -18)}
