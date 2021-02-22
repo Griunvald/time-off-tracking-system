18a68753133ee8be5453d84dfe6be39f856b5a5e
Vitali Griunvald <vgriunvald@gmail.com>
2021-02-22 23:32:13 +0200

    Create empty AdminDashboard component, render on login for admin user

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import RequestForm from './form/RequestForm';
 import MyRequests from './MyRequests';
+import AdminDashboard from './AdminDashboard';
+import { useSelector } from 'react-redux';
 
 const Home = () => {
-  return (
-    <>
-      <RequestForm />
-      <MyRequests />
-    </>
-  );
+  const currentUser = useSelector((state) => state.auth.currentUser);
+  if (currentUser && currentUser.email === 'admin@mail.com') {
+    return (
+      <>
+        <AdminDashboard />
+      </>
+    );
+  } else {
+    return (
+      <>
+        <RequestForm />
+        <MyRequests />
+      </>
+    );
+  }
 };
 
 export default Home;
