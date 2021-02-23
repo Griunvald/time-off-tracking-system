ecda4fc2e78e8975abe589c0a46b3e8251ce01a7
Vitali Griunvald <vgriunvald@gmail.com>
2021-02-23 19:47:20 +0200

    Get all user requests and console.log() for admin

--- client/src/components/AdminDashboard.js
+++ client/src/components/AdminDashboard.js
@@ -1,6 +1,33 @@
 import React from 'react';
+import firebase from './../config/firebase';
+import { firebaseLooper } from '../utils/firebaseLooper';
+
+const db = firebase.firestore();
 
 const AdminDashboard = () => {
+  const emailList = [];
+  const requstsList = [];
+  const usersRef = db.collection('users');
+
+  usersRef.get().then((snapshot) => {
+    const emails = firebaseLooper(snapshot);
+    emails.forEach((el) => {
+      emailList.push(el.email);
+      const requestsRef = db
+        .collection('users_requests')
+        .doc(el.email)
+        .collection(el.email);
+
+      requestsRef.get().then((snapshot) => {
+        const requests = firebaseLooper(snapshot);
+        if (requests.length > 0) {
+          requstsList.push(...requests);
+        }
+      });
+    });
+    console.log(requstsList);
+  });
+
   return <div>Admin's Dashboard</div>;
 };
 
