8def6ac6909163f6f902cb741c4ff265e029086e
Vitali Griunvald <vgriunvald@gmail.com>
2021-02-23 23:55:48 +0200

    [bug] Render some of the user requests on the admin dashboard

--- client/src/components/AdminDashboard.js
+++ client/src/components/AdminDashboard.js
@@ -1,17 +1,21 @@
 import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import firebase from './../config/firebase';
 import { firebaseLooper } from '../utils/firebaseLooper';
+import { Table, Label } from 'semantic-ui-react';
 
 const db = firebase.firestore();
+const moment = require('moment');
 
 const AdminDashboard = () => {
+  const currentUser = useSelector((state) => state.auth.currentUser);
   const dispatch = useDispatch();
 
   useEffect(() => {
     const emailList = [];
     const requstsList = [];
     const usersRef = db.collection('users');
+
     usersRef.get().then((snapshot) => {
       const emails = firebaseLooper(snapshot);
       emails.forEach((el) => {
@@ -32,8 +36,63 @@ const AdminDashboard = () => {
       console.log(requstsList);
     });
   }, [dispatch]);
+  const allRequests = useSelector((state) => state.admin.requests);
 
-  return <div>Admin's Dashboard</div>;
+  if (currentUser && allRequests.length !== 0) {
+    return (
+      <div>
+        <h3 style={{ padding: '80px 0 20px' }}>Admin Dashbord</h3>
+        <Table celled stripped="true">
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell>Requested</Table.HeaderCell>
+              <Table.HeaderCell>From</Table.HeaderCell>
+              <Table.HeaderCell>To</Table.HeaderCell>
+              <Table.HeaderCell>Status</Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+            {allRequests.map &&
+              allRequests.map((item) => (
+                <Table.Row key={item.id}>
+                  <Table.Cell>
+                    {moment
+                      .unix(item.createdAt.seconds)
+                      .toString()
+                      .slice(3, -18)}
+                  </Table.Cell>
+                  <Table.Cell>
+                    {moment
+                      .unix(item.startDate.seconds)
+                      .toString()
+                      .slice(0, -18)}
+                  </Table.Cell>
+                  <Table.Cell>
+                    {moment.unix(item.endDate.seconds).toString().slice(0, -18)}
+                  </Table.Cell>
+                  <Table.Cell>
+                    <Label
+                      horizontal
+                      color={
+                        item.status === 'pending'
+                          ? 'yellow'
+                          : item.status === 'approved'
+                          ? 'green'
+                          : 'red'
+                      }
+                    >
+                      {item.status}
+                    </Label>
+                  </Table.Cell>
+                </Table.Row>
+              ))}
+          </Table.Body>
+        </Table>
+      </div>
+    );
+  } else {
+    return null;
+  }
 };
 
 export default AdminDashboard;
